889afea9be1754624f3a834072b496a8152cbe28
supascooopa <mnalptekin1@gmail.com>
2023-08-02 15:09:59 +0300

    loading website and clicking cookie

--- clicker.py
+++ clicker.py
@@ -3,14 +3,22 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
+# loading website
 driver = webdriver.Firefox()
 driver.get("https://orteil.dashnet.org/cookieclicker/")
-
-# TODO QUICK 1. Consent to cookie usage, Switch to iframe
 time.sleep(5)
-driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]")
-# TODO QUICK 2. Choose language
-# TODO 1. click cookie
+
+# Consenting to cookie usage
+consent_button = driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]")
+consent_button.click()
+
+# QUICK 2. Choose language
+time.sleep(2)
+language_button = driver.find_element(By.XPATH, '//*[@id="langSelect-EN"]')
+language_button.click()
+
+# click cookie
+time.sleep(2)
 cookie = driver.find_element(By.ID, "bigCookie")
 cookie.click()
 
